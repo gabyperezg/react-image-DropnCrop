050c18e0d80a676154c00297c1a79afc07efb8bc
PichuPerez <gabyperez.dev@gmail.com>
2020-04-10 19:52:29 -0500

    Updates

---
+++ precache-manifest.72deb7d1898ed0ddeef512c1bfe46275.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158425c27f6668b10986ab600c1a7d63",
+    "url": "/react-image-DropnCrop/index.html"
+  },
+  {
+    "revision": "b0cb2e2f2da79e4c617b",
+    "url": "/react-image-DropnCrop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9930f48de634134c417d",
+    "url": "/react-image-DropnCrop/static/js/2.207e65b2.chunk.js"
+  },
+  {
+    "revision": "9b257d4073f9428215a1abd8a285b1da",
+    "url": "/react-image-DropnCrop/static/js/2.207e65b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cb2e2f2da79e4c617b",
+    "url": "/react-image-DropnCrop/static/js/main.9ed202d4.chunk.js"
+  },
+  {
+    "revision": "d0117d71d7c6068becfc",
+    "url": "/react-image-DropnCrop/static/js/runtime-main.d3b62731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-image-DropnCrop/static/media/logo.5d5d9eef.svg"
+  }
+]);
